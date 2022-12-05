c426ecbc71bfb1fa62dce3b2a2fc68a83ca35609
ceri540 <chensy57@mail2.sysu.edu.cn>
2022-12-05 13:52:49 +0800

    basci struct of uvadlc

---
+++ framw_work_model/general/task.py
@@ -0,0 +1,10 @@
+"""
+ @Time    : 2022/12/05 
+ @Author  : siyu chen
+ @email   : chensy57@mail2.sysu.edu.cn
+
+ @project : pytorch module setting framework
+ @File    : general/task.py
+ @Function: train steps and validation steps
+
+"""
---
+++ framw_work_model/general/train.py
@@ -0,0 +1,13 @@
+# contain basic operator: training for loops; load/save model; set models
+"""
+ @Time    : 2022/12/05 
+ @Author  : siyu chen
+ @email   : chensy57@mail2.sysu.edu.cn 
+ 
+ @project : pytorch module setting framework
+ @File    : general/train.py
+ @Function: basic operator
+            - training for loops
+            - load/save model
+            - set models
+"""
